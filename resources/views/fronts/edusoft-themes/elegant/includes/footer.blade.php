14d838ad981068e15a56f688906c44b454e18f54
AdeotiDev <adeoti4biz@gmail.com>
2024-11-10 05:27:33 +0100

    Started the front pages B

--- resources/views/fronts/edusoft-themes/elegant/includes/footer.blade.php
+++ resources/views/fronts/edusoft-themes/elegant/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container">
-        <p>&copy; {{ date('Y') }} School Name. All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} {{$schoolDetails['school_name']}}. All rights reserved.</p>
         <div class="social-links">
             <a href="https://facebook.com" target="_blank">Facebook</a>
             <a href="https://twitter.com" target="_blank">Twitter</a>
