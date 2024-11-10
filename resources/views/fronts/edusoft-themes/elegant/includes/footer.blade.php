b4d10e3976fb2f7379774ae95f8c7f32f9402c99
AdeotiDev <adeoti4biz@gmail.com>
2024-11-10 09:46:36 +0100

    Started the front pages F

--- resources/views/fronts/edusoft-themes/elegant/includes/footer.blade.php
+++ resources/views/fronts/edusoft-themes/elegant/includes/footer.blade.php
@@ -1,10 +1,21 @@
 <footer class="footer">
     <div class="container">
-        <p>&copy; {{ date('Y') }} {{$schoolDetails['school_name']}}. All rights reserved.</p>
-        <div class="social-links">
-            <a href="https://facebook.com" target="_blank">Facebook</a>
-            <a href="https://twitter.com" target="_blank">Twitter</a>
-            <a href="https://instagram.com" target="_blank">Instagram</a>
+       <div class="row">
+        <div class="col-6">
+            <p>&copy; {{ date('Y') }} {{$schoolDetails['school_name']}}. All rights reserved.</p>
+            <div class="social-links">
+                <a href="https://facebook.com" target="_blank">Facebook</a>
+                <a href="https://twitter.com" target="_blank">Twitter</a>
+                <a href="https://instagram.com" target="_blank">Instagram</a>
+            </div>
+            
         </div>
+        <div class="col-6">
+            <ul style="list-style-type: none; text-align:left;">
+                <li><b>Phone:</b> {{$schoolDetails['school_phone']}}</li>
+                <li><b>Address:</b> {{$schoolDetails['school_address']}}</li>
+            </ul>
+        </div>
+       </div>
     </div>
 </footer>
