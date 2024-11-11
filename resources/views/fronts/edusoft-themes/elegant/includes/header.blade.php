bd37972862d5b8b0222d0cafc15e594c765fb2a7
AdeotiDev <adeoti4biz@gmail.com>
2024-11-11 07:02:07 +0100

    Started the front pages I

--- resources/views/fronts/edusoft-themes/elegant/includes/header.blade.php
+++ resources/views/fronts/edusoft-themes/elegant/includes/header.blade.php
@@ -1,18 +1,20 @@
-
+    @php
+        $school_logo = $schoolDetails['school_logo'];
+    @endphp
     <!-- Header Section with Bootstrap Navbar -->
     <header class="header bg-white sticky-top shadow-sm">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <!-- Logo -->
                 <a class="navbar-brand" href="#">
-                    <img src="logo.png" alt="Logo" class="logo-img" style="height: 50px;">
+
+                    <img src="{{ Storage::url($school_logo) }}" alt="Logo" class="logo-img" style="height: 50px; border-radius: 10%;">
                 </a>
     
                 <!-- Mobile Toggle Button -->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-    
                 <!-- Navbar Links -->
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
