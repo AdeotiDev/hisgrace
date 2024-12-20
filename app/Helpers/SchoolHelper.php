940773318eba38742a37072a241c4dea23ff2cd5
AdeotiDev Al-Faoz <adeotitempo@gmail.com>
2024-12-20 13:13:18 +0100

    Update SchoolHelper.php
    Altered the school_helpers

--- app/Helpers/SchoolHelper.php
+++ app/Helpers/SchoolHelper.php
@@ -8,17 +8,17 @@ use App\Models\Teacher;
 if (!function_exists('getSchoolDetails')) {
     function getSchoolDetails()
     {
-        $school = Setting::firstOrFail();
+        // $school = Setting::firstOrFail();
 
         return [
-            'school_name' => $school->school_name,
-            'school_address' => $school->address,
-            'school_phone' => $school->contact,
-            'school_logo' => $school->logo,
-            'school_favicon' => $school->favicon,
-            'meta_description' => $school->meta_description,
-            'meta_title' => $school->meta_title,
-            'meta_keywords' => $school->meta_keywords,
+            'school_name' => "",//$school->school_name,
+            'school_address' => "",//$school->address,
+            'school_phone' => "",//$school->contact,
+            'school_logo' => "",//$school->logo,
+            'school_favicon' => "",//$school->favicon,
+            'meta_description' => "",//$school->meta_description,
+            'meta_title' => "",//$school->meta_title,
+            'meta_keywords' => "",//$school->meta_keywords,
         ];
     }
 
@@ -29,15 +29,15 @@ if (!function_exists('getSchoolDetails')) {
 
         function getSchoolStats(){
 
-            $totalStudents = Student::count();
-            $totalTeachers = Teacher::count();
-            $totalClasses =  SchoolClass::count();
+            // $totalStudents = Student::count();
+            // $totalTeachers = Teacher::count();
+            // $totalClasses =  SchoolClass::count();
 
 
             return [
-                'totalStudents' => number_format($totalStudents),
-                'totalTeachers' => number_format($totalTeachers),
-                'totalClasses' => number_format($totalClasses)
+                'totalStudents' => "",//number_format($totalStudents),
+                'totalTeachers' => "",//number_format($totalTeachers),
+                'totalClasses' => "",//number_format($totalClasses)
             ];
         }
 
